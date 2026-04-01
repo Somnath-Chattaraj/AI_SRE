8e0359ce7876a74fb301ab6411c392ec0976b8d9
dasakash26 <akashd2664@gmail.com>
2026-04-01 10:51:54 +0530

    Fix: INC-20260329_065426

--- application/src/database.js
+++ application/src/database.js
@@ -1,7 +1,4 @@
 export const simulateDatabaseHang = (req, res) => {
     console.warn("WARN: Upstream database connection timeout");
-    // Wait 4 seconds before responding
-    setTimeout(() => {
-        res.status(504).send('Gateway Timeout. Database is locked.');
-    }, 4000);
+    res.status(504).send('Gateway Timeout. Database is locked.');
 };
